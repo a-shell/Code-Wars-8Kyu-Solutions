043d4e2426457e8bb1780f22f225ae80011187c2
Andrew Shellington <andrew.shellington@hotmail.com>
2022-04-11 22:01:27 -0400

    Added comments to find# & removeElements

--- findNumber.js
+++ findNumber.js
@@ -1,10 +1,13 @@
 //A function to find the first non-consecutive number in an array
 function firstNonConsecutive (arr) {
+    //Create a for loop
     for (let i = 0; i < arr.length - 1; i++){
+    //If the index following the current index minus the current index is not equal to one, return the following index
       if(arr[i + 1] - arr[i] !== 1){
         return arr[i + 1];
       }; 
     };
+    //After iterating through the array, if all numbers are consecutive return null
     return null; 
   }
   
@@ -12,7 +15,10 @@ function firstNonConsecutive (arr) {
 //Alt solutions
 function firstNonConsecutive2 (arr) {
   for (let i = 1; i < arr.length; i++){
-    if (arr[i -1] + 1 !== arr[i]) return arr[i];
+    if (arr[i - 1] + 1 !== arr[i]) return arr[i];
   }
   return null;
 }
+
+firstNonConsecutive([1,2,3,4,6,7,8]) //6
+firstNonConsecutive([1,2,3,4,5,6,7]) //null
--- removingElements.js
+++ removingElements.js
@@ -1,4 +1,4 @@
-//A function to remove every other element from an array
+//A function to remove second other element from an array
 function removeEveryOther(arr){
     //your code here
     return arr.filter((x, i) => i % 2 === 0);
@@ -7,11 +7,17 @@ function removeEveryOther(arr){
 //Alt solutions
 function removeEveryOther2(arr){
     //your code here
+    //Create a new array to store the affected values
     let newArr = [];
+    //Create a For loop
     for (let i = 0; i < arr.length; i++){
+    //If the index is divisible by 2 (in this example, the "keep" elements), push that element to the new array
       if (i % 2 == 0){
         newArr.push(arr[i]);
       };
     };
+    //Return the new array made up of the "keep" elements"
     return newArr;
     }
+
+    removeEveryOther2(["Keep", "Remove", "Keep", "Remove", "Keep"]) // ["Keep","Keep","Keep"]
